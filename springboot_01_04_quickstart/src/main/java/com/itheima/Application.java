d8891fd75b697c7d800f0599aa13a49c493cfbab
YG <710431075@qq.com>
2023-01-02 20:36:10 +0800

    手动pom+手动引导类

---
+++ springboot_01_04_quickstart/src/main/java/com/itheima/Application.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+/*
+@author YG
+@create 2023/1/2   20:26
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ springboot_01_04_quickstart/src/main/java/com/itheima/controller/BookController.java
@@ -0,0 +1,19 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+@author YG
+@create 2023/1/2   20:31
+*/
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @GetMapping
+    public String getById(){
+        System.out.println("SpringBoot is running...2");
+        return "SpringBoot is running...2";
+    }
+}
