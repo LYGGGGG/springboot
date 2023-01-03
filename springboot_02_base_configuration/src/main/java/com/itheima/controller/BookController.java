cf9a7991434a0c0c18f28721dc37fffcd5cbca5f
YG <710431075@qq.com>
2023-01-03 19:41:29 +0800

    yml配置

--- springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
+++ springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.itheima.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    // 读取yml数据中的单一数据
+    @Value("${lession}")
+    private String lession;
+
+    @Value("${user.name1}")
+    private String userName;
+
+    @Autowired
+    private Environment env;
+
     @GetMapping
     public String getById() {
         System.out.println("springboot is running...");
+        System.out.println("lession===>" + lession);
+        System.out.println("userName===>" + userName);
+        System.out.println("---------------------------");
+        System.out.println(env.getProperty("lession"));
         return "springboot is running... port = 80";
     }
 
