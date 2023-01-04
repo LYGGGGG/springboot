36588b261cca66d2ebc142593a6e559a7d6e1ad5
YG <710431075@qq.com>
2023-01-04 14:09:01 +0800

    springboot整合mybatis

--- springboot_05_mybatis/src/main/java/com/itheima/Springboot05MybatisApplication.java
+++ springboot_05_mybatis/src/main/java/com/itheima/Springboot05MybatisApplication.java
@@ -1,9 +1,11 @@
-package com.itheima.springboot_05_mybatis;
+package com.itheima;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.itheima.dao")
 public class Springboot05MybatisApplication {
 
 	public static void main(String[] args) {
