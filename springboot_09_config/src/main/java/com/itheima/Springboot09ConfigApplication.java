d790eff87992c0cc2006717bfe5783c68212b827
YG <710431075@qq.com>
2023-02-04 23:45:18 +0800

    多个配置文件

--- springboot_09_config/src/main/java/com/itheima/Springboot09ConfigApplication.java
+++ springboot_09_config/src/main/java/com/itheima/Springboot09ConfigApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot09ConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot09ConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot09ConfigApplication.class, args);
+    }
 
 }
--- springboot_09_config/src/test/java/com/itheima/Springboot09ConfigApplicationTests.java
+++ springboot_09_config/src/test/java/com/itheima/Springboot09ConfigApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Springboot09ConfigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
