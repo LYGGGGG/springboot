96e13d5b07f21acdfa36c1a7b29d911f9cb83810
YG <710431075@qq.com>
2023-02-04 17:03:17 +0800

    前端页面

--- springboot_08_ssmp/src/main/java/com/itheima/Springboot08SsmpApplication.java
+++ springboot_08_ssmp/src/main/java/com/itheima/Springboot08SsmpApplication.java
@@ -3,10 +3,13 @@ package com.itheima;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class Springboot08SsmpApplication {
 
 	public static void main(String[] args) {
+		System.out.println(Arrays.toString(args));
 		SpringApplication.run(Springboot08SsmpApplication.class, args);
 	}
 
